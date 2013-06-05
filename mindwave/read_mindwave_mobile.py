18d99cc809e696941bf544ba00c60e65bcee1075
evee746 <evee746@gmail.com>
2013-06-05 16:20:54 -0700

    Change script to not crash when headset disconnected in the middle of recording -- now retries every 5s

--- mindwave/read_mindwave_mobile.py
+++ mindwave/read_mindwave_mobile.py
@@ -17,31 +17,35 @@ if __name__ == '__main__':
     data = {}
 
     while(True):
-        dataPoint = mindwaveDataPointReader.readNextDataPoint()
-        if (not dataPoint.__class__ is RawDataPoint):
-            print dataPoint
-            if dataPoint.__class__ is EEGPowersDataPoint:
-                for name in EEGPowersDataPoint.WAVE_NAMES:
+        try:
+            dataPoint = mindwaveDataPointReader.readNextDataPoint()
+            if (not dataPoint.__class__ is RawDataPoint):
+                print dataPoint
+                if dataPoint.__class__ is EEGPowersDataPoint:
+                    for name in EEGPowersDataPoint.WAVE_NAMES:
+                        if name not in headers:
+                            print "*!*!*!*!*!!*!* WTF is this data point?", name
+                            continue
+                        data[name] = getattr(dataPoint, name)
+                else:
+                    name, value = dataPoint.name, dataPoint.value
                     if name not in headers:
                         print "*!*!*!*!*!!*!* WTF is this data point?", name
                         continue
-                    data[name] = getattr(dataPoint, name)
-            else:
-                name, value = dataPoint.name, dataPoint.value
-                if name not in headers:
-                    print "*!*!*!*!*!!*!* WTF is this data point?", name
-                    continue
-                if name in data:
-                    print "<-> Overwriting %s value from %d to %d" % (
-                        name, data[name], value)
-                data[name] = value
-            if len(data) >= len(headers):
-                values = [data[h] for h in headers]
-                line = str(time.time()) + ',' + ','.join(str(x) for x in values)
-                print "len values:", len(values)
-                print values
-                print >>f, line
-                f.flush()
-                data = {}
-            else:
-                print "len data is %d, len headers is %d" % (len(data), len(headers))
+                    if name in data:
+                        print "<-> Overwriting %s value from %d to %d" % (
+                            name, data[name], value)
+                    data[name] = value
+                if len(data) >= len(headers):
+                    values = [data[h] for h in headers]
+                    line = str(time.time()) + ',' + ','.join(str(x) for x in values)
+                    print "len values:", len(values)
+                    print values
+                    print >>f, line
+                    f.flush()
+                    data = {}
+                else:
+                    print "len data is %d, len headers is %d" % (len(data), len(headers))
+        except bluetooth.btcommon.BluetoothError, e:
+            print "Bluetooth Error (Headset off?), retrying in 5s"
+            time.sleep(5)
