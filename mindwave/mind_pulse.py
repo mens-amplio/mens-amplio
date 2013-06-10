82784b072cd7cac958c65f39b31e51bcac15edbb
evee746 <evee746@gmail.com>
2013-06-10 16:15:35 -0700

    Minor bug printing colors to the screen

--- mindwave/mind_pulse.py
+++ mindwave/mind_pulse.py
@@ -1,6 +1,13 @@
 #!/usr/bin/python
 
-'''Mind-controlled LEDs. Spooky.'''
+'''Mind-controlled LEDs. Spooky.
+
+Colors the LedStripWS2801 LEDs according to the Mindwave 'attention' reading:
+* Green if your attention is maxiimal
+* Red if your attention is minimal
+* Blue if you're in the middle
+* White if the headset is not ready
+'''
 
 import sys
 PATH_TO_PULSE_FOLDER = "/home/pi/pulse-test/"
@@ -8,27 +15,20 @@ sys.path.append(PATH_TO_PULSE_FOLDER)
 from LedStrip_WS2801 import LedStrip_WS2801 as LedStrip
 from mindwave import Headset
 
-def exaggerate(level, maxvalue, exponent=0.4):
-  halfmax = maxvalue/2.0
-  level -= halfmax
-  negative = level < 0
-  level = abs(level) ** exponent
-  if negative: level = -level
-  adjustment = halfmax ** exponent
-  level += adjustment
-  level = maxvalue * level / (2 * adjustment)
-  return level
 
 led_strip = LedStrip("/dev/spidev0.0", 20)
 headset = Headset()
 while True:
   point = headset.readDatapoint()
   print "Attention:", point.attention
-  colors = (0, 255, 0)  # G B R
+  colors = (255, 255, 255)  # G B R
   if point.headsetOnHead():
-    a = exaggerate(point.attention, 100, 0.4)
-    greenlevel = int(255.0 * a / 100)
-    colors = (greenlevel, 0, 255 - greenlevel)  # G B R
+    if point.attention > 66:
+      colors = (255, 0, 0)  # Green for paying attention
+    elif point.attention < 33:
+      colors = (0, 0, 255)  # Red for slacking off
+    else:
+      colors = (0, 255, 0)  # Blue for in-the-middle
   print "Coloring LEDS to RGB:", (colors[2], colors[0], colors[1])
   led_strip.setAll(colors)
   led_strip.update()
