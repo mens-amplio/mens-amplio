7958798f68b804f70ef152b30c0f8f66caec4dfc
evee746 <evee746@gmail.com>
2013-06-10 15:35:47 -0700

    Adjust color response

--- mindwave/mind_pulse.py
+++ mindwave/mind_pulse.py
@@ -8,15 +8,15 @@ sys.path.append(PATH_TO_PULSE_FOLDER)
 from LedStrip_WS2801 import LedStrip_WS2801 as LedStrip
 from mindwave import Headset
 
-led_strip = LedStrip("/dev/spidev0.0", 6)
+led_strip = LedStrip("/dev/spidev0.0", 20)
 headset = Headset()
 while True:
   point = headset.readDatapoint()
   print point
   if point.headsetOnHead():
-    brightness = int(255*point.attention/100.)
+    a = int(255*point.attention/100.)
     led_strip.setAll((  # G B R
-      brightness, brightness, brightness))
+      a, 0, 255-a))
   else:
-    led_strip.setAll((255, 255, 255))  # G B R
+    led_strip.setAll((0, 255, 0))  # G B R
   led_strip.update()
