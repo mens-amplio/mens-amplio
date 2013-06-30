297620986b55d4b42201c3fc893793eeb30bd547
Micah Elizabeth Scott <beth@scanlime.org>
2013-06-30 14:47:40 -0700

    Keep LED order consistent in graph_to_layout

--- modeling/graph_to_layout.py
+++ modeling/graph_to_layout.py
@@ -6,6 +6,8 @@ A graph JSON file defines an object g with two fields:
   - g.nodes is an object whose values are [x, y, z] triples
   - g.edges is an object whose values are pairs of keys in the g.nodes object
 
+Keys in g.nodes are LED indices which will match the indices used in OPC.
+
 An OPC layout file is a JSON array where each element has one of these forms:
   - {"point": [x, y, z]}
   - {"line": [[x1, y1, z1], [x2, y2, z2]]}
@@ -25,7 +27,18 @@ if len(args) != 2:
 
 graph = json.load(open(args[0]))
 nodes = dict((int(k), scale(v)) for k, v in graph['nodes'].items())
-layout = [{'line': [nodes[i], nodes[j]]} for i, j in graph['edges'].values()]
+
+# Sort edges by LED indices (converted to integers)
+edges = [(int(k), v) for k, v in graph['edges'].items()]
+edges.sort()
+
+# Warn if the indices don't seem to be contiguous
+print "Found %d LEDs" % len(edges)
+for i, (k, v) in enumerate(edges):
+    if i != k:
+        print "*** LEDs not in order? (Index %d has label %d)" % (i, k)
+
+layout = [{'line': [nodes[i], nodes[j]]} for k, (i, j) in edges]
 
 file = open(args[1], 'w')
 json.dump(layout, file)
