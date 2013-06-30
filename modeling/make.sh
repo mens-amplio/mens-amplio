d54699c049e54a74adf597dae826dda394c2a2e7
Jesse Wolfe <jes5199@gmail.com>
2013-06-30 15:41:48 -0700

    fix text in make.sh

--- modeling/make.sh
+++ modeling/make.sh
@@ -1,11 +1,11 @@
 #/bin/bash
 set -e
 echo "✨ HEY, LISTEN!✨ "
-echo "This script assumes that you've already manually exported curve_endpoints.txt from Rhino"
+echo "This script assumes that you've already manually exported rod_endpoints.txt from Rhino"
 echo "You can do that in Rhino by selecting Tools > Command > Search..."
 echo "and typing RunPythonScript"
 echo "when the first file open dialog box appears, use it to select exportRodEndpoints.py"
-echo "when the second file open dialog box appears, use it to select curve_endpoints.txt for overwriting"
+echo "when the second file open dialog box appears, use it to select rod_endpoints.txt for overwriting"
 
 echo
 echo "Converting rod_endpoints.txt to graph.data.json"
