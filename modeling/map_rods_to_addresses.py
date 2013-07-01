998314bd85758a0a1ed9cc050a5ff82442950db7
Ari Lacenski <alacenski@gmail.com>
2013-06-30 18:05:21 -0700

    Picks out lowest-level stems.

--- modeling/map_rods_to_addresses.py
+++ modeling/map_rods_to_addresses.py
@@ -9,6 +9,18 @@
 # its parent rod's address
 # and the addresses of its children.
 
+# file prerequisites:
+# graph.data.json, a dict of nodes and a dict of edges that captures connections between nodes
+
+"""
+in graph.data.json, a given stem 0 is described by nodes 0 and 1.
+
+1 is the upper end node of the group.
+do any other edges (other than 0) include the 1 node as a FIRST (beginning) node?
+if so, those nodes have stem[0]'s level plus 1
+
+an edge is on level 0 if: no nodes contain its first node id as a second node
+"""
 import sys
 import json
 
@@ -16,6 +28,20 @@ import json
 from q import q
 from pprint import pprint
 
+# Functions
+def edge_is_root(id, edges):
+    edge_bottom_node = edges[id][0]
+    for test_id, test_nodes in edges.iteritems():
+        if id == test_id:
+            continue
+        if test_nodes[1] == edge_bottom_node:
+            return False
+
+    # made it this far, so it must be a root node!
+    return True
+
+
+# File input
 args = sys.argv[1:]
 infile = None
 
@@ -25,10 +51,24 @@ except:
     print "No source file was provided."
     exit()
 
-infile_points, output_rods = [], []
-infile_points = json.load(infile)
 
-pprint(infile_points)
-# parse the input file
-#for pair in infile_points:
-#    q(pair)
+infile_json = json.load(infile)
+infile_edges = infile_json['edges']
+possible_root_edges = infile_edges.copy()
+root_edge_ids = []
+branch_edge_ids = []
+
+# split up edges into roots and branches
+for edge_id, nodes in infile_edges.iteritems():
+    if edge_is_root(edge_id, possible_root_edges):
+        root_edge_ids.append(edge_id)
+    else:
+        branch_edge_ids.append(edge_id)
+
+
+
+# Tasks.
+# 1. From z-axes, the greater z-axis value is the up end
+# 2. If the z-axes for the pair are identical, then the rod is horizontal and there are different rules to parse its tree
+# If the z-axes are not identical, then the rod is vertical enough to judge by vertical levels
+
