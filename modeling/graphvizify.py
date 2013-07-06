21379a2c1aa2ee730075cf9622a5074858a0fe45
Jesse Wolfe <jes5199@gmail.com>
2013-07-05 19:47:41 -0700

    update graphviz to match addresses

--- modeling/graphvizify.py
+++ modeling/graphvizify.py
@@ -2,13 +2,16 @@
 import json
 
 data = json.load(open("graph.data.json"))
+remap = json.load(open("manual.remap.json"))
+
+address_of_led = {str(v):k for k,v in remap.items()}
 
 print "digraph {"
 print "  graph [splines=false];"
-for node_id in sorted(data["nodes"], key=int):
-  print "  ", node_id, "[shape=point,label=\"\"]", ";"
-for edge_id in sorted(data["edges"], key=int):
+for edge_id in sorted(data["edges"], key=address_of_led.get):
   edge = data["edges"][edge_id]
   start, stop = edge
   print "  ", start, " -> ", stop, "[arrowhead=none,label=\""+edge_id+"\"]", ";"
+for node_id in sorted(data["nodes"], key=int):
+  print "  ", node_id, "[shape=point,label=\"\"]", ";"
 print "}"
