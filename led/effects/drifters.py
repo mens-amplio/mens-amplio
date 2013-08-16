e8c05c85742c3624cf8aed8149857e931a566333
Jesse Wolfe <jes5199@gmail.com>
2013-08-16 02:46:51 -0700

    MinimalColorDrifter
    this has a much simpler render logic - it will probably be easier to port to C.

--- led/effects/drifters.py
+++ led/effects/drifters.py
@@ -91,6 +91,45 @@ class ColorDrifterLayer(EffectLayer):
     def render(self, model, params, frame):
         raise NotImplementedError("Implement render in ColorDrifterLayer subclass")
 
+class MinimalColorDrifter(EffectLayer):
+    fadeSteps = 255
+    def __init__(self, colors, switchTime):
+        self.switchTime = switchTime
+        self.rgb_colors = numpy.array(colors)
+        self.hsv_colors = matplotlib.colors.rgb_to_hsv(self.rgb_colors.reshape(-1,1,3)).reshape(-1,3)
+        self.color_count = len(self.hsv_colors)
+        self.secondsPerCycle = self.switchTime * len(colors)
+        self.secondsPerFadeColor = float(self.secondsPerCycle) / self.fadeSteps / self.color_count
+        self.totalSteps = self.fadeSteps * len(colors)
+        self.precalc()
+
+    def precalc(self):
+        self.fade_colors_hsv = numpy.zeros([self.totalSteps, 3])
+
+        steps = numpy.tile(numpy.arange(0, 1, 1.0/self.fadeSteps), 3)
+        indices = numpy.arange(3).repeat(self.fadeSteps)
+        for i in range(self.color_count):
+            pair_of_colors = self.hsv_colors[[i, (i+1)%self.color_count ]]
+
+            # Hue is a loop, this is how to force the shortest path
+            if(pair_of_colors[0,0] - pair_of_colors[1,0] > 0.5):
+                pair_of_colors[1,0] += 1
+            elif(pair_of_colors[0,0] - pair_of_colors[1,0] < -0.5):
+                pair_of_colors[1,0] -= 1
+            interpolater = scipy.interpolate.RectBivariateSpline([0,1], [0,1,2], pair_of_colors, kx=1, ky=1)
+            between_colors = interpolater.ev(steps, indices).reshape(3, -1)
+            between_colors[0] = between_colors[0] % 1 # return hue to 0..1
+            between_colors = between_colors.swapaxes(0,1)
+            self.fade_colors_hsv[range(i*self.fadeSteps,(i+1)*self.fadeSteps)] = between_colors
+        self.fade_colors_rgb = matplotlib.colors.hsv_to_rgb(self.fade_colors_hsv.reshape(-1,1,3)).reshape(-1,3)
+
+    def getFadeColor(self, time):
+        index = int( (time % self.secondsPerCycle) / self.secondsPerFadeColor )
+        return self.fade_colors_rgb[index]
+
+    def render(self, model, params, frame):
+        c = self.getFadeColor(params.time)
+        numpy.add(frame, c, frame)
 
 class HomogenousColorDrifterLayer(ColorDrifterLayer):    
     """ Color drift is homogenous across the whole brain """
